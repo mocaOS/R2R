1460f9ae5bd44531d445bf47fce78542a02f4384
Tobias Thiele <creazy231@users.noreply.github.com>
2025-04-15 12:08:07 +0200

    Update create-hatchet-db.sh

--- docker/scripts/create-hatchet-db.sh
+++ docker/scripts/create-hatchet-db.sh
@@ -2,14 +2,14 @@
 
 set -e
 echo 'Waiting for PostgreSQL to be ready...'
-while ! pg_isready -h hatchet-postgres -p 5432 -U ${HATCHET_POSTGRES_USER:-hatchet_user}; do
+while ! pg_isready -h hatchet-postgres -p 5432 -U "${HATCHET_POSTGRES_USER}"; do
   sleep 1
 done
 
 echo 'PostgreSQL is ready, checking if database exists...'
-if ! PGPASSWORD=${HATCHET_POSTGRES_PASSWORD:-hatchet_password} psql -h hatchet-postgres -p 5432 -U ${HATCHET_POSTGRES_USER:-hatchet_user} -lqt | grep -qw ${HATCHET_POSTGRES_DBNAME:-hatchet}; then
+if ! PGPASSWORD="${HATCHET_POSTGRES_PASSWORD}" psql -h hatchet-postgres -p 5432 -U "${HATCHET_POSTGRES_USER}" -lqt | grep -qw "${HATCHET_POSTGRES_DBNAME}"; then
   echo 'Database does not exist, creating it...'
-  PGPASSWORD=${HATCHET_POSTGRES_PASSWORD:-hatchet_password} createdb -h hatchet-postgres -p 5432 -U ${HATCHET_POSTGRES_USER:-hatchet_user} -w ${HATCHET_POSTGRES_DBNAME:-hatchet}
+  PGPASSWORD="${HATCHET_POSTGRES_PASSWORD}" createdb -h hatchet-postgres -p 5432 -U "${HATCHET_POSTGRES_USER}" -w "${HATCHET_POSTGRES_DBNAME}"
 else
   echo 'Database already exists, skipping creation.'
 fi
